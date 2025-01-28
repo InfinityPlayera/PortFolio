6123bbcaecc365ce77191e63e379d7106fa52e31
InfinityPlayera <infinityplayera@gmail.com>
2025-01-27 16:48:56 -0800

    Initial commit

---
+++ src/Details.js
@@ -0,0 +1,181 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import vue from "./assets/techstack/vue.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import angular from "./assets/techstack/angular.png";
+import solidity from "./assets/techstack/solidity.png";
+import rust from "./assets/techstack/rust.png";
+import nft from "./assets/techstack/nft.png";
+import defi from "./assets/techstack/defi.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Sai Uno",
+  tagline: "I build things for web",
+  img: profile,
+  about: `With an extensive background spanning over eight years, he has honed his skills as a highly skilled full-stack developer, specializing in React and solidity development. Proficient in translating intricate designs into meticulously crafted code, he exemplifies a commitment to excellence in design-to-code implementation. His expertise extends to implementing highly responsive user interface components using advanced React and solidity concepts, ensuring optimal user experiences across web and blockchain platforms. Focused primarily on web and blockchain development, he remains dedicated to staying abreast of industry trends and emerging technologies. His unwavering dedication to writing clean and reusable code, coupled with a keen adherence to best practices, underscores his commitment to delivering high-quality solutions that exceed expectations.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  telegram: "https://t.me/InfiniteColin",
+  github: "https://github.com/infinityplayera",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Full Stack Developer",
+    Company: `Sansan Inc`,
+    Location: "Tokyo, Japan",
+    Type: "Full Time",
+    Duration: "July 2023 - PRESENT",
+  },
+  {
+    Position: "Blockchain Developer",
+    Company: `Oasis`,
+    Location: "London, England",
+    Type: "Part Time",
+    Duration: "May 2022 - June 2023",
+  },
+  {
+    Position: "Blockchain Developer",
+    Company: `The Pokémon Company, Tokyo, Japan`,
+    Location: "Tokyo, Japan",
+    Type: "Full Time",
+    Duration: "October 2020 - January 2022",
+  },
+  {
+    Position: "Full Stack Engineer",
+    Company: `Avatar Engines`,
+    Location: "Mannheim, Germany",
+    Type: "Full Time",
+    Duration: "November 2018 - October 2020",
+  },
+  {
+    Position: "Solidity Developer",
+    Company: `Krypteia`,
+    Location: "Sydney, Australia",
+    Type: "Full Time",
+    Duration: "Augest 2018 - October 2019",
+  },
+  {
+    Position: "Frontend Developer",
+    Company: `Profico`,
+    Location: "Split, Croatia",
+    Type: "Part Time",
+    Duration: "February 2017 - August 2018",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Engineering",
+    Company: "nara institute of science and technology",
+    Location: "Ikoma, Japan",
+    Type: "Full Time",
+    Duration: "February 2012 - November 2016",
+  }
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  vue: vue,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  angular: angular,
+  solidity: solidity,
+  rust: rust,
+  nft: nft,
+  defi: defi,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "knownorigin",
+    image: projectImage1,
+    description: `At KnownOrigin, we are passionate about empowering artists and collectors in the digital art space. Founded in 2018, our platform is dedicated to celebrating creativity and innovation through the use of blockchain technology.`,
+    techstack: "ERC721, React, Node.js"
+  },
+  {
+    title: "superrare",
+    image: projectImage2,
+    description: `At SuperRare, we believe that art is a powerful medium for expression and connection. Founded in 2018, our platform is dedicated to showcasing and selling unique digital artworks as non-fungible tokens (NFTs) on the blockchain.`,
+    techstack: "REC721, React, Node.js"
+  },
+  {
+    title: "donwoodford",
+    image: projectImage3,
+    description: `Whether you are waiting for your vehicle to get serviced, picking up a part for your vehicle, or stopping by to check out a new vehicle for yourself, we offer a wide range of amenities to enhance your experience here! Come by, look around and check what we can offer you!
+`,
+    techstack: "React, Node.js",
+  },
+  {
+    title: "argosfarm",
+    image: projectImage4,
+    description: `Martin Cattle Company is a family owned and operated beef operation focused on sustainable practices and quality production. After almost a decade of planning, preparing and practicing, The Martin Family is proud to offer Quarter, Half and Whole Beef Shares raised on the lush pasture of Central Kentucky Bluegrass and available for pickup at our home in NJ. `,
+    techstack: "React, Bootstrap"
+  },
+  {
+    title: "Godaddy",
+    image: projectImage5,
+    description: `GoDaddy does more than sell domain names. We help millions of small businesses globally accelerate their growth, giving entrepreneurs confidence at every stage of their business journey. People come to GoDaddy to build their business. Our global solutions seamlessly connect their identity and presence with commerce, leading to profitable growth.`,
+    techstack: "React, PHP"
+  },
+  {
+    title: "resideat2727",
+    image: projectImage6,
+    description: `Contact us today by phone or send a note using our contact form. One of our leasing professionals is on standby and ready to assist you. We look forward to hearing from you!`,
+    techstack: "Vue.js, Wordpress"
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "SoftengSkill@.com",
+};
---
+++ src/Pages/Home.js
@@ -0,0 +1,98 @@
+import React, { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { personalDetails } from "../Details";
+
+function Home() {
+  const { name, tagline, img } = personalDetails;
+  const h11 = useRef();
+  const h12 = useRef();
+  const h13 = useRef();
+  const myimageref = useRef();
+  useEffect(() => {
+    const tl = gsap.timeline();
+    tl.from(
+      h11.current,
+      {
+        x: "-100%",
+        delay: 0.8,
+        opacity: 0,
+        duration: 2,
+        ease: "Power3.easeOut",
+      },
+      "<"
+    )
+      .from(
+        h12.current,
+        {
+          x: "-100%",
+          delay: 0.5,
+          opacity: 0,
+          duration: 2,
+          ease: "Power3.easeOut",
+        },
+        "<"
+      )
+      .from(
+        h13.current,
+        {
+          x: "-100%",
+          delay: 0.1,
+          opacity: 0,
+          duration: 2,
+          ease: "Power3.easeOut",
+        },
+        "<"
+      )
+      .from(
+        myimageref.current,
+        {
+          x: "200%",
+          delay: 0.5,
+          opacity: 0,
+          duration: 2,
+          ease: "Power3.easeOut",
+        },
+        "<"
+      );
+  }, []);
+
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = `/assets/resume/Sai_Uno_Resume.pdf`; // Use PUBLIC_URL to access static files
+    link.download = 'resume.pdf'; // The name the file will be downloaded as
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  return (
+    <main className="container mx-auto max-width section md:flex justify-between items-center">
+      <div>
+        <h1
+          ref={h11}
+          className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+        >
+          Hi,👋<br></br>My Name is<br></br>
+        </h1>
+        <h1
+          ref={h12}
+          className="text-2xl bg-clip-text bg-gradient text-transparent md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+        >
+          {name}
+        </h1>
+        <h2
+          ref={h13}
+          className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+        >
+          <div className="mb-[32px]">{tagline}</div>
+          <button onClick={handleDownload} className="bg-dark-heading text-white border-[3px] border-solid py-[8px] px-[32px] text-center inline-block text-[24px] my-[4px] mx-[2px] cursor-pointer rounded-[10px] hover:bg-white hover:bg-gray-500">Resume</button>
+        </h2>
+      </div>
+      <div className="mt-5 md:mt-0">
+        <img ref={myimageref} className="w-1/2 md:ml-auto" src={img} alt="Pavan MG" />
+      </div>
+    </main>
+  );
+}
+
+export default Home;
